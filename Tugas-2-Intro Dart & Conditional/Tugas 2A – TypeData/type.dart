1bf14709526b82d976d313212f89ae29b1e4256e
muhith7 <abdulmuhithnawawi@gmail.com>
2024-07-16 23:41:00 +0700

    Pengumpulan Tugas 2

---
+++ Tugas-2-Intro Dart & Conditional/Tugas 2A – TypeData/type.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+void main() {
+  // Soal No. 1
+  // var word = 'dart';
+  // var second = 'is';
+  // var third = 'awesome';
+  // var fourth = 'and';
+  // var fifth = 'I';
+  // var sixth = 'love';
+  // var seventh = 'it!';
+  //
+  // final gabung = '$word $second $third $fourth $fifth $sixth $seventh';
+  // print(gabung);
+
+  // Soal No. 2
+  // var sentence = "I am going to be Flutter Developer";
+  //
+  // var exampleFirstWord = sentence[0] ;
+  // var exampleSecondWord = sentence[2] + sentence[3] ;
+  // var thirdWord = sentence[5] + sentence[6] + sentence[7] + sentence[8] + sentence[9];
+  // var fourthWord = sentence[11] + sentence[12];
+  // var fifthWord = sentence[14] + sentence[15];
+  // var sixthWord = sentence[17] + sentence[18] + sentence[19] + sentence[20] + sentence[21] + sentence[22] + sentence[23];
+  // var seventhWord = sentence[25] + sentence[26] + sentence[27] + sentence[28] + sentence[29] + sentence[30] + sentence[31] + sentence[32] + sentence[33];
+  //
+  //
+  // print('First Word: ' + exampleFirstWord);
+  // print('Second Word: ' + exampleSecondWord);
+  // print('Third Word: ' + thirdWord);
+  // print('Fourth Word: ' + fourthWord);
+  // print('Fifth Word: ' + fifthWord);
+  // print('Sixth Word: ' + sixthWord);
+  // print('Seventh Word: ' + seventhWord);
+
+  // Soal No. 3
+  // print('masukan nama depan:');
+  // String?inputNamaDepan = stdin.readLineSync()!;
+  // print("masukan nama belakang:");
+  // String?inputNamaBelakang = stdin.readLineSync()!;
+  // print("nama lengkap anda adalah:");
+  // print('$inputNamaDepan  $inputNamaBelakang');
+
+  // Soal No. 4
+  // var a = 5;
+  // var b = 10;
+  //
+  // var perkalian = a*b;
+  // var pembagian = a/b;
+  // var penambahan = a+b;
+  // var pengurangan = a-b;
+  //
+  // print('perkalian : $perkalian');
+  // print('pembagian : $pembagian');
+  // print('penambahan : $penambahan');
+  // print('pengurangan : $pengurangan');
+
+
+
+}
---
+++ Tugas-2-Intro Dart & Conditional/Tugas-2B– Conditional/conditional.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+void main() {
+  // 1. Ternary operator
+  // print('apakah anda yakin akan meng-install dart ?');
+  // String?inputJawaban = stdin.readLineSync()!;
+  // inputJawaban == 'y' ? print('anda akan menginstall aplikasi dart') : (inputJawaban == 't' ? print('aborted') : print('gagal'));
+
+  // 2. if-else if dan else
+  // print('masukan nama anda di werewolf world :');
+  // String?inputNama = stdin.readLineSync()!;
+  // print('masukan peran anda di werewolf world :');
+  // String?inputPeran = stdin.readLineSync()!;
+  //
+  // if(inputNama == ''){
+  //   print('Nama harus diisi!');
+  // }else if(inputPeran == ''){
+  //   print('Halo $inputNama, Pilih peranmu untuk memulai game!');
+  // }else if(inputPeran == 'Penyihir'){
+  //   print('Selamat datang di Dunia Werewolf, $inputNama');
+  //   print('Halo Penyihir $inputNama, kamu dapat melihat siapa yang menjadi werewolf!');
+  // }else if(inputPeran == 'Guard'){
+  //   print('Selamat datang di Dunia Werewolf, $inputNama');
+  //   print('Halo Guard $inputNama, kamu akan membantu melindungi temanmu dari serangan werewolf.');
+  // }else if(inputPeran == 'Werewolf'){
+  //   print('Selamat datang di Dunia Werewolf, $inputNama');
+  //   print('Halo Werewolf $inputNama, Kamu akan memakan mangsa setiap malam!');
+  // }
+
+  // 3. Switch Case
+  // print('saya memiliki syair yang bagus, sebutkan saja apa hari ini :');
+  // String?inputHari = stdin.readLineSync()!;
+  // switch(inputHari) {
+  //     case 'senin': { print('Segala sesuatu memiliki kesudahan, yang sudah berakhir biarlah berlalu dan yakinlah semua akan baik-baik saja.'); break;}
+  //     case 'selasa': { print('Setiap detik sangatlah berharga karena waktu mengetahui banyak hal, termasuk rahasia hati.'); break;}
+  //     case 'rabu': { print('Jika kamu tak menemukan buku yang kamu cari di rak, maka tulislah sendiri.'); break;}
+  //     case 'kamis': { print('Jika hatimu banyak merasakan sakit, maka belajarlah dari rasa sakit itu untuk tidak memberikan rasa sakit pada orang lain.'); break;}
+  //     case 'jumat': { print('Hidup tak selamanya tentang pacar.'); break;}
+  //     case 'sabtu': { print('Rumah bukan hanya sebuah tempat, tetapi itu adalah perasaan.'); break;}
+  //     case 'minggu': { print('Hanya seseorang yang takut yang bisa bertindak berani. Tanpa rasa takut itu tidak ada apapun yang bisa disebut berani.'); break;}
+  //   default:  { print('Masukan hari dengan huruf kecil semua'); }
+  // }
+
+  //4. Switch Case
+  // var tanggal = 5;
+  // var bulan = 5;
+  // var tahun = 2003;
+  //
+  // var namaBulan;
+  //
+  // switch (bulan) {
+  //   case 1:
+  //     namaBulan = "Januari";
+  //     break;
+  //   case 2:
+  //     namaBulan = "Februari";
+  //     break;
+  //   case 3:
+  //     namaBulan = "Maret";
+  //     break;
+  //   case 4:
+  //     namaBulan = "April";
+  //     break;
+  //   case 5:
+  //     namaBulan = "Mei";
+  //     break;
+  //   case 6:
+  //     namaBulan = "Juni";
+  //     break;
+  //   case 7:
+  //     namaBulan = "Juli";
+  //     break;
+  //   case 8:
+  //     namaBulan = "Agustus";
+  //     break;
+  //   case 9:
+  //     namaBulan = "September";
+  //     break;
+  //   case 10:
+  //     namaBulan = "Oktober";
+  //     break;
+  //   case 11:
+  //     namaBulan = "November";
+  //     break;
+  //   case 12:
+  //     namaBulan = "Desember";
+  //     break;
+  //   default:
+  //     namaBulan = "Bulan tidak valid";
+  // }
+  //
+  // print('$tanggal $namaBulan $tahun');
+
+
+
+
+}
