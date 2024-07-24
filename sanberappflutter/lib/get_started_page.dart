bb046231377d59fce1f79442c170485536a3d04d
muhith7 <abdulmuhithnawawi@gmail.com>
2024-07-24 21:19:40 +0700

    Tugas Slacing Halaman Get Started

---
+++ sanberappflutter/lib/get_started_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 86),
+              height: 330,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/news_paper.png')
+                  )
+              ),
+            ),
+            SizedBox(height: 37),
+            Text("Yuk, Membaca Bersama \n Sanber News",
+              style:
+            GoogleFonts.arimo(fontSize: 21, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 21),
+            Text("Berita Terpercaya, Di Ujung Jari Anda",
+            style: TextStyle(fontSize: 15),
+            ),
+            Spacer(),
+            SizedBox(
+              height: 52,
+              width: 354,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Masuk",
+                  style: GoogleFonts.arimo(
+                    fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
+
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff3498DB),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
+                  ),
+                ),
+            ),
+            SizedBox(height: 21,),
+            SizedBox(
+              height: 52,
+              width: 354,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Mendaftar',
+                  style: GoogleFonts.arimo(
+                      fontSize: 15, fontWeight: FontWeight.bold),
+
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8),
+                    side: const BorderSide(color: Color(0xff3498DB)))
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
